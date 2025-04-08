508df049893bbe10adc770cb6e063aab094b3492
Pablo Ariel De Los Santos <pdelossantos@nosis.com>
2025-04-08 14:55:56 -0300

    Feat: UseState y ClassState

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { UseState } from "./UseState.jsx";
+import { ClassState } from "./ClassState.jsx";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <UseState />
+      <ClassState />
+    </div>
+  );
+}
+
+export default App;
